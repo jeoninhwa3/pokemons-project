64643b43a6eb260d34fd3551f3e7e04bcbbef451
inhwa <jeoninhwa3@naver.com>
2024-07-02 16:36:11 +0900

    style: RootLayout 구성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      height: {
+        "calc-100vh-minus-100px": "calc(100vh - 100px)",
       },
     },
   },
